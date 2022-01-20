012415ca90da506cbdb97854fd4d757f3da0d391
muhammed9295 <mdsheikh9295@gmail.com>
2022-01-20 13:36:37 +0400

    version 2(attendance)

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import './app.css';
-import Sidebar from './components/sidebar/Sidebar';
-import Topbar from './components/topbar/Topbar';
-import AgentDashboard from './pages/agentDashboard/AgentDashboard';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,  
-} from "react-router-dom";
-import BirdEyeView from './pages/birdEyeView/BirdEyeView';
-import NewStaffEntry from './pages/Staffs/newStaffEntry/NewStaffEntry';
-import UserAccess from './pages/userAccess/UserAccess';
-import NewUser from './pages/userAccess/NewUser';
-import StaffAttendance from './pages/Staffs/staffAttendance/StaffAttendance';
-import AddProjects from './pages/projects/addNewProjects/AddProjects';
-import MassMigration from './pages/projects/massMigration/MassMigration';
-import Staffdb from './pages/Staffs/staffDatabase/Staffdb';
-
+import React from "react";
+import "./app.css";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import AgentDashboard from "./pages/agentDashboard/AgentDashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BirdEyeView from "./pages/birdEyeView/BirdEyeView";
+import NewStaffEntry from "./pages/Staffs/newStaffEntry/NewStaffEntry";
+import UserAccess from "./pages/userAccess/UserAccess";
+import NewUser from "./pages/userAccess/NewUser";
+import StaffAttendance from "./pages/Staffs/staffAttendance/StaffAttendance";
+import AddProjects from "./pages/projects/addNewProjects/AddProjects";
+import MassMigration from "./pages/projects/massMigration/MassMigration";
+import Staffdb from "./pages/Staffs/staffDatabase/Staffdb";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    
     <Router>
       <Topbar />
+      {/* <Login /> */}
       <div className='container'>
-          
-          <Sidebar /> 
-            <Routes>
-                <Route path="/project-overview"  element={<AgentDashboard />}/>
-                <Route path="/" exact element={<BirdEyeView />}/>
-                <Route path="/add-new-staff" element={<NewStaffEntry />}/>
-                <Route path="/user-access" element={<UserAccess />}/>
-                <Route path="/create-new-user" element={<NewUser />}/>
-                <Route path="/staff-attendance" element={<StaffAttendance />}/>
-                <Route path="/add-new-projects" element={<AddProjects />}/>
-                <Route path="/mass-migration" element={<MassMigration />}/>
-                <Route path="/staff-database" element={<Staffdb />}/>
-            </Routes>      
+        <Sidebar />
+        <Routes>
+          <Route path='/project-overview' element={<AgentDashboard />} />
+          <Route path='/' exact element={<BirdEyeView />} />
+          <Route path='/add-new-staff' element={<NewStaffEntry />} />
+          <Route path='/user-access' element={<UserAccess />} />
+          <Route path='/create-new-user' element={<NewUser />} />
+          <Route path='/staff-attendance' element={<StaffAttendance />} />
+          <Route path='/add-new-projects' element={<AddProjects />} />
+          <Route path='/mass-migration' element={<MassMigration />} />
+          <Route path='/staff-database' element={<Staffdb />} />
+        </Routes>
       </div>
-     
     </Router>
-    
   );
 }
 
